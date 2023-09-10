61d13219051f7ee0e54eede8ee6f94f164984308
Ekaterina_P <zazie25@yandex.ru>
2023-09-11 01:24:21 +0300

    file added

--- ProductDatabaseException.java
+++ ProductDatabaseException.java
@@ -1,2 +1,5 @@
-package PACKAGE_NAME;public class ProductDatabaseException {
-}
+public class ProductDatabaseException extends Exception{
+    public ProductDatabaseException(String message) {
+            super(message);
+        }
+    }
