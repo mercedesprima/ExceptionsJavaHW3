9fadd9815dd286036ac2707a44e63a3365b61554
Ekaterina_P <zazie25@yandex.ru>
2023-09-11 01:24:47 +0300

    file added

--- ShopManager.java
+++ ShopManager.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class ShopManager {
+import java.util.List;
+
+public class ShopManager {
+    static ProductDatabase database = new ProductDatabase();
+    public ShopManager(ProductDatabase database) {
+        this.database = database;
+    }
+    public static double purchaseProduct(int productId, int quantity) throws ProductDatabaseException, ShopManagerException {
+        Product product = database.getProduct(productId);
+        if (product.getAvailableQuantity() < quantity){
+            throw new ShopManagerException("Insufficient quantity");
+        }
+        double totalPrice = product.getPrice() * quantity;
+        database.updateInventory(product, quantity);
+        return totalPrice;
+    }
+
+    public static List<Product> getInventory() {
+        return database.getInventory();
+    }
 }
