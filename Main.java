de05e0b01d36f72c3e41aae32e1a87cf05a03138
Ekaterina_P <zazie25@yandex.ru>
2023-09-11 01:21:49 +0300

    file added

--- Main.java
+++ Main.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class Main {
+
+public class Main {
+    public static void main(String[] args) {
+        ProductDatabase database = new ProductDatabase();
+        ShopManager manager = new ShopManager(database);
+        OnlineShopUI onlineShopUI = new OnlineShopUI();
+        onlineShopUI.start();
+    }
 }
