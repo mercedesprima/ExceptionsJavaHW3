efd9d8daf69e7250a2c0b8675e86a0f1a95df3dd
Ekaterina_P <zazie25@yandex.ru>
2023-09-11 01:23:09 +0300

    file added

--- Product.java
+++ Product.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Product {
+public class Product {
+    private String name;
+    private int availableQuantity;
+    private double price;
+
+    @Override
+    public String toString() {
+        return String.format("Name: %s, Price: %f, Available Quantity: %d", name, price,availableQuantity);
+    }
+
+    public Product(String name, int availableQuantity, double price) {
+        this.name = name;
+        this.availableQuantity = availableQuantity;
+        this.price = price;
+    }
+    public int getAvailableQuantity() {
+        return availableQuantity;
+    }
+    public void setAvailableQuantity(int availableQuantity) {
+        this.availableQuantity = availableQuantity;
+    }
+    public double getPrice() {
+        return price;
+    }
 }
